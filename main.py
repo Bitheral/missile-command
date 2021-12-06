f3fdda79b7ef3c1b2e58a4bf085bfa9ccf0856c6
Bitheral <me@bitheral.net>
2021-12-06 12:29:53 +0000

    Fixed player missiles
    Player missiles now follow Bresenham algo

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ white = (255, 255, 255)
 
 width, height = 1024, 768
 screen = None
+clock = None
 
 maxRadius = 60
 explosions = []
-delay = 100  # number of milliseconds delay before generating a USEREVENT
 
 ground_height = height - 32
 
@@ -31,61 +31,57 @@ class Bresenham:
     def __init__(self, p0, p1):
         self.initial = True
         self.end = False
-        self.start = p0
-        self.target = p1
+        self.p0 = p0
+        self.p1 = p1
+        self.x0 = p0[0]
+        self.y0 = p0[1]
+        self.x1 = p1[0]
+        self.y1 = p1[1]
+        self.dx = abs(self.x1 - self.x0)
+        self.dy = abs(self.y1 - self.y0)
 
-        self.start_x = self.start[0]
-        self.start_y = self.start[1]
-
-        self.target_x = self.target[0]
-        self.target_y = self.target[1]
-
-        self.dx = abs(self.target_x - self.start_x)
-        self.dy = abs(self.target_y - self.start_y)
-
-        if self.start_x < self.target_x:
+        if self.x0 < self.x1:
             self.sx = 1
         else:
             self.sx = -1
 
-        if self.start_y < self.target_y:
+        if self.y0 < self.y1:
             self.sy = 1
         else:
             self.sy = -1
-
         self.err = self.dx - self.dy
-
-    def get_current_position(self):
-        return [self.start_x, self.start_y]
 
     def get_next(self):
         if self.initial:
             self.initial = False
-            return [self.start_x, self.start_y]
+            return [self.x0, self.y0]
 
-        if self.start_x == self.target_x and self.start_y == self.target_y:
+        if self.x0 == self.x1 and self.y0 == self.y1:
             self.end = True
-            return [self.target_x, self.target_y]
+            return [self.x1, self.y1]
 
-        if self.err * 2 > -self.dy:
-            self.err -= self.dy
-            self.start_x -= self.sx
-        if self.err * 2 < self.dx:
-            self.err += self.dx
-            self.start_y += self.sy
+        self.e2 = 2 * self.err
+        if self.e2 > -self.dy:
+            self.err = self.err - self.dy
+            self.x0 = self.x0 + self.sx
+        if self.e2 < self.dx:
+            self.err = self.err + self.dx
+            self.y0 = self.y0 + self.sy
+        return [self.x0, self.y0]
 
-        self.get_current_position()
+    def get_current_pos(self):
+        return [self.x0, self.y0]
 
     def finished(self):
         return self.end
 
 
 class Explosion:
-    def __init__(self, pos, maxRadius):
+    def __init__(self, pos, max_radius):
         self.pos = pos
         self.radius = random.randrange(1, 10)
         self.increasing = True
-        self.maxRadius = maxRadius
+        self.maxRadius = max_radius
 
     def draw(self):
         if self.radius > 0:
@@ -113,23 +109,22 @@ class Missile:
         self.width = 8
         self.height = 16
         self.path = Bresenham(self.pos, self.target)
-
-        self.missile_verticies = [(self.x, self.y), (self.x + (self.width / 2), self.y + (self.height / 2)),
-                                  (self.x + self.width, self.y),
-                                  (self.x + (self.width / 2), self.y + (self.height / 4))]
+        self.start_position = start
 
     def draw(self):
-        pygame.draw.polygon(screen, (255, 255, 255), self.missile_verticies)
+        pygame.draw.circle(screen, (255, 255, 255), self.pos, 2)
+        pygame.draw.line(screen, (255, 255, 255), self.pos, self.start_position)
 
     def update(self):
-        if self.pos == self.target:
-            global missiles
-            missiles.remove(this)
-
         if not self.path.finished():
             self.pos = self.path.get_next()
+        else:
+            createExplosion(self.pos, 80)
 
-    ## TODO: Implement movement code
+            if self in player_missiles:
+                player_missiles.remove(self)
+            else:
+                attack_missiles.remove(self)
 
 
 class Silo:
@@ -146,7 +141,7 @@ class Silo:
 
         self.silo_rect = pygame.Rect(self.x + self.height, self.y - self.height - 4,
                                      self.width - (self.height + self.height), height - self.height)
-        self.launchPosition = [self.x + (self.height / 2), self.y - self.height]
+        self.launchPosition = [self.x + (self.width / 2), self.y - self.height]
 
     def getLaunchPosition(self):
         return self.launchPosition
@@ -224,59 +219,6 @@ silos = [Silo((width / 8 + 96, ground_height), 128),
          Silo((width / 2 - ((width / 8) / 2) + width / 8 + 64, ground_height), 128)]
 
 
-#
-# class explosion:
-#     def __init__ (self, pos):
-#         self._radius = 1
-#         self._maxRadius = maxRadius
-#         self._increasing = True
-#         self._pos = pos
-#
-#     def update (self):
-#         if self._increasing:
-#             pygame.draw.circle (screen, white, self._pos, self._radius, 0)
-#             self._radius += 1
-#             if self._radius == self._maxRadius:
-#                 self._increasing = False
-#         else:
-#             pygame.draw.circle (screen, black, self._pos, self._radius, 0)
-#             self._radius -= 1
-#             if self._radius > 0:
-#                 pygame.draw.circle (screen, white, self._pos, self._radius, 0)
-#             else:
-#                 globalRemove (self)
-#
-# def createExplosion (pos):
-#     global allExplosions
-#     allExplosions += [explosion (pos)]
-#     pygame.time.set_timer (USEREVENT+1, delay)
-#
-#
-# def globalRemove (e):
-#     global allExplosions
-#     allExplosions.remove (e)
-#
-# def updateAll ():
-#     if allExplosions != []:
-#         for e in allExplosions:
-#             e.update ()
-#         pygame.display.flip ()
-#         pygame.time.set_timer (USEREVENT+1, delay)
-#
-# def wait_for_event ():
-#     global screen
-#     while True:
-#         event = pygame.event.wait ()
-#         if event.type == pygame.QUIT:
-#             sys.exit(0)
-#         if event.type == KEYDOWN and event.key == K_ESCAPE:
-#             sys.exit (0)
-#         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-#             createExplosion (pygame.mouse.get_pos ())
-#         if event.type == USEREVENT+1:
-#             updateAll ()
-#
-
 def createExplosion(pos, radius):
     global explosions
     explosions += [Explosion(pos, radius)]
@@ -297,22 +239,23 @@ def createPlayerMissile():
 
 
 def main():
-    global screen
+    global screen, clock
     pygame.init()
     screen = pygame.display.set_mode([width, height])
+    clock = pygame.time.Clock()
 
     while True:
         screen.fill((128, 127, 255))
-        event = pygame.event.wait()
-        if event.type == pygame.QUIT:
-            sys.exit(0)
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            createExplosion(pygame.mouse.get_pos(), 5)
-            createPlayerMissile()
-            for city in cities:
-                if not city.destroyed:
-                    city.damage(pygame.mouse.get_pos())
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                createPlayerMissile()
+                for city in cities:
+                    if not city.destroyed:
+                        city.damage(pygame.mouse.get_pos())
 
         for city in cities:
             city.draw()
@@ -331,10 +274,7 @@ def main():
             silo.draw()
 
         pygame.display.flip()
-        pygame.time.set_timer(USEREVENT + 1, 100)
-    # wait_for_event ()
+        clock.tick(120)
 
-
-# :)
 
 main()
