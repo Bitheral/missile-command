8071546f7e25e8efbf464fbd454ea733dadac5d5
Bitheral <me@bitheral.net>
2021-12-07 17:25:13 +0000

    Finished up
    All features implemented - Take a look at assignment cover to check if covered all sections

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-import pygame, sys, time, random, math
+import math
+import pygame
+import random
+import sys
 from pygame.locals import *
 
 ramp_one, ramp_two, ramp_three = None, None, None
@@ -15,7 +18,7 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 width, height = 1024, 768
-screen = None
+screen = pygame.Surface((width, height))
 clock = None
 
 maxRadius = 60
@@ -27,6 +30,7 @@ attack_missiles = []
 player_missiles = []
 
 last_update = 0
+last_spawn = 0
 
 
 class Bresenham:
@@ -41,6 +45,7 @@ class Bresenham:
         self.y1 = p1[1]
         self.dx = abs(self.x1 - self.x0)
         self.dy = abs(self.y1 - self.y0)
+        self.e2 = 0
 
         if self.x0 < self.x1:
             self.sx = 1
@@ -79,36 +84,47 @@ class Bresenham:
 
 
 class Explosion:
-    def __init__(self, pos, max_radius):
+    def __init__(self, pos, max_radius, parent):
         self.pos = pos
         self.radius = random.randrange(1, 10)
         self.increasing = True
         self.maxRadius = max_radius
         self.rect = pygame.Rect(self.pos[0], self.pos[1], self.radius, self.radius)
+        self.parent = parent
+        self.causedByPlayer = parent.isPlayer
+        self.speed = 0.5
 
     def draw(self):
+        global screen
         if self.radius > 0:
             pygame.draw.circle(screen, (255, 255, 255), self.pos, self.radius, 0)
+        else:
+            del self
 
     def in_range(self, other):
         return math.sqrt((self.pos[0] - other.pos[0]) ** 2 + (self.pos[1] - other.pos[1]) ** 2) <= (
-                self.radius * 2)
+            self.radius * 2)
+
+    def in_max_range(self, other):
+        return math.sqrt((self.pos[0] - other.pos[0]) ** 2 + (self.pos[1] - other.pos[1]) ** 2) <= (
+            self.maxRadius * 2)
 
     def update(self):
         if self.radius >= self.maxRadius:
             self.increasing = False
+
         elif self.radius < 0:
             global explosions
             explosions.remove(self)
 
         if self.increasing:
-            self.radius += 1
+            self.radius += 1 * self.speed
         else:
-            self.radius -= 1
+            self.radius -= 1 * self.speed
 
 
 class Missile:
-    def __init__(self, start, destination, radius=10):
+    def __init__(self, start, destination, radius=10, is_player=False):
         self.pos = start
         self.target = destination
         self.x = self.pos[0]
@@ -117,15 +133,21 @@ class Missile:
         self.path = Bresenham(self.pos, self.target)
         self.start_position = start
         self.rect = Rect(self.pos[0], self.pos[1], self.radius, self.radius)
+        self.isPlayer = is_player
 
     def explode(self):
-        createExplosion(self.pos, 80)
+        createExplosion(self.pos, 80, self)
 
     def collide(self, other):
         if not self == other:
             return self.rect.contains(other.rect) or self.rect.colliderect(other.rect)
 
+    def in_range(self, other, threshold):
+        return math.sqrt((self.pos[0] - other.pos[0]) ** 2 + (self.pos[1] - other.pos[1]) ** 2) <= (
+                (self.radius + threshold) * 2)
+
     def draw(self):
+        global screen
         pygame.draw.circle(screen, (255, 255, 255), self.pos, self.radius)
         pygame.draw.line(screen, (255, 255, 255), self.pos, self.start_position)
 
@@ -142,7 +164,10 @@ class Missile:
 
 
 class Silo:
+    global screen
+
     def __init__(self, pos, w):
+        global last_update
         self.pos = pos
         self.missiles = 6
         self.width = w
@@ -156,17 +181,32 @@ class Silo:
         self.silo_rect = pygame.Rect(self.x + self.height, self.y - self.height - 4,
                                      self.width - (self.height + self.height), height - self.height)
         self.launchPosition = [self.x + (self.width / 2), self.y - self.height]
+        self.reload_time = 0
 
     def getLaunchPosition(self):
         return self.launchPosition
+
+    def update(self):
+        max_missiles = 6
+
+        if last_update -self.reload_time >= 1500 and self.missiles < max_missiles:
+            self.missiles += 1
 
     def draw(self):
         # pygame.draw.rect(screen, (255, 255, 255), self.rect)
         pygame.draw.polygon(screen, (0, 255, 0), self.mound_vertices)
         pygame.draw.rect(screen, (64, 64, 64), self.silo_rect)
 
+        ammo_radius = 4
+
+        for ammo in range(self.missiles):
+            ammo_pos = (self.x + (self.height + ((ammo_radius * 2) + ammo_radius / 2) * ammo) + ammo_radius * 1.5, self.y + self.height / 2)
+            pygame.draw.circle(screen, (255, 255, 255),ammo_pos , ammo_radius, 0)
+
 
 class City:
+    global screen
+
     def __init__(self, pos, city_width):
         self.pos = pos
         self.width = city_width
@@ -199,16 +239,11 @@ class City:
             self.buildings.append(building_dict)
 
     def damage(self):
-        global destroyed_cities
-
         if sum(b["destroyed"] for b in self.buildings) == len(self.buildings):
             self.destroyed = True
         else:
             rand_index = random.randrange(len(self.buildings))
             building = self.buildings[rand_index]
-            while building["destroyed"]:
-                rand_index = random.randrange(len(self.buildings))
-                building = self.buildings[rand_index]
 
             if not building["destroyed"]:
                 building["destroyed"] = True
@@ -235,9 +270,9 @@ silos = [Silo((width / 8 + 96, ground_height), 128),
          Silo((width / 2 - ((width / 8) / 2) + width / 8 + 64, ground_height), 128)]
 
 
-def createExplosion(pos, radius):
+def createExplosion(pos, radius, parent):
     global explosions
-    explosions += [Explosion(pos, radius)]
+    explosions += [Explosion(pos, radius, parent)]
 
 
 def createPlayerMissile():
@@ -247,17 +282,21 @@ def createPlayerMissile():
     for silo in silos:
         distance = math.hypot(silo.launchPosition[0] - pygame.mouse.get_pos()[0],
                               silo.launchPosition[1] - pygame.mouse.get_pos()[1])
-        if distance < smallest_distance:
+        if distance < smallest_distance and silo.missiles > 0:
             smallest_distance = distance
             closest_silo = silo
 
-    player_missiles += [Missile(closest_silo.launchPosition, pygame.mouse.get_pos())]
+    if closest_silo is not None:
+        if closest_silo.missiles > 0:
+            player_missiles += [Missile(closest_silo.launchPosition, pygame.mouse.get_pos(), is_player=True)]
+            closest_silo.missiles -= 1
+            closest_silo.reload_time = pygame.time.get_ticks()
 
 
 def createAttackMissile():
-    global cities, attack_missiles, clock, last_update
+    global cities, attack_missiles, clock, last_update, last_spawn
 
-    delay = 1000
+    max_missiles = 5
     now = pygame.time.get_ticks()
 
     start_x = random.randint(0, width)
@@ -266,7 +305,6 @@ def createAttackMissile():
 
     smallest_distance = math.inf
     closest_city = None
-    end_x = 0
     for city in cities:
         distance = math.hypot(city.center[0] - start_x,
                               city.center[1] - start_y)
@@ -281,13 +319,13 @@ def createAttackMissile():
 
     end = [end_x, ground_height]
 
-    if now - last_update >= delay and len(attack_missiles) < 8:
-        last_update = now
+    if now - last_spawn >= 1500 and len(attack_missiles) < max_missiles:
         attack_missiles += [Missile(start, end)]
+        last_spawn = now
 
 
 def main():
-    global screen, clock
+    global screen, clock, last_update
     pygame.init()
     screen = pygame.display.set_mode([width, height])
     clock = pygame.time.Clock()
@@ -295,7 +333,13 @@ def main():
     while True:
         screen.fill((128, 127, 255))
 
-        createAttackMissile()
+        destroyed_cities = [city for city in cities if city.destroyed]
+
+        if not destroyed_cities == cities:
+            createAttackMissile()
+        else:
+            for attack in attack_missiles:
+                attack.explode()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -312,7 +356,7 @@ def main():
             missile.draw()
             missile.update()
             for attack in attack_missiles:
-                if missile.collide(attack):
+                if missile.in_range(attack, -5):
                     missile.explode()
                     attack.explode()
                     player_missiles.remove(missile)
@@ -326,9 +370,7 @@ def main():
             explosion.draw()
             explosion.update()
             for city in cities:
-                # if city.rect.colliderect(explosion.rect):
-                #     city.damage()
-                if explosion.in_range(city):
+                if explosion.in_max_range(city) and not explosion.causedByPlayer:
                     city.damage()
 
             for attack in attack_missiles:
@@ -340,10 +382,11 @@ def main():
 
         for silo in silos:
             silo.draw()
+            silo.update()
 
-        last_update = pygame.time.get_ticks()
         pygame.display.flip()
         clock.tick(120)
+        last_update = pygame.time.get_ticks()
 
 
 main()
